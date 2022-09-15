3f9ba13df54dc934a9e042406d0d1c3edc9de146
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-09-16 02:09:15 +0600

    initially complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar } from './assets/components/Navbar';
+import { Product } from './assets/components/Product';
 
 export const App = () => {
+  const [productQuantity, setProductQuantity] = useState(0);
+  const [cartProductQuantity, setCartProductQuantity] = useState(0);
   return (
     <>
-      <Navbar />
+      <Navbar
+        cartProductQuantity={cartProductQuantity}
+        setCartProductQuantity={setCartProductQuantity}
+      />
+      <Product
+        productQuantity={productQuantity}
+        setProductQuantity={setProductQuantity}
+        setCartProductQuantity={setCartProductQuantity}
+      />
     </>
   );
 };
